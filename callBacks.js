6baacec4de24bbf1f9ea6463a910ac932386172d
vinaypatel1437 <patel.vp.vinay@gmail.com>
2022-06-21 21:29:23 +0530

    Fact

--- callBacks.js
+++ callBacks.js
@@ -33,4 +33,31 @@ function a(){
 a();
 
 
+setTimeout(()=>{
+    let fact= 1;
+    for(let i=1;i<=5;i++){
+        fact *= i;
+    }
+    console.log(fact);
+},4000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
